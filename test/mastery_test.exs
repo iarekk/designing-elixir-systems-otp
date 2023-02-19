f0c12742c8cf92088fdf30cd1efcccd1e0ee4a92
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-02-19 15:09:50 +0000

    response module

--- test/mastery_test.exs
+++ test/mastery_test.exs
@@ -5,4 +5,15 @@ defmodule MasteryTest do
   test "greets the world" do
     assert Mastery.hello() == :world
   end
+
+  test "can create a question" do
+    r = %Mastery.Core.Question{
+      asked: "Hello?",
+      substitutions: %{"1" => "2"}
+    }
+
+    assert r.asked == "Hello?"
+
+    assert r.substitutions["1"] == "2"
+  end
 end
