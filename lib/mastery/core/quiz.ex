29867a71744bb39baf5e3e13c957c0b0181586e7
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-02-20 23:16:05 +0000

    what is what in the quiz

--- lib/mastery/core/quiz.ex
+++ lib/mastery/core/quiz.ex
@@ -8,3 +8,15 @@ defmodule Mastery.Core.Quiz do
             record: %{},
             mastered: []
 end
+
+# title (String.t)                                The title for a quiz.
+# mastery (integer)                               The number of questions a user must get right to master a quiz category.
+#
+# metadata as users advance through the quiz:
+#
+# current_question (Question.t)                   The current question being presented to the user.
+# last_response (Response.t)                      The last response given by the user.
+# templates (%{ "category" => [Template.t]})      The master list of templates, by category.
+# used ([Template.t])                             The templates that we’ve used, this cycle, that have not yet been mastered.
+# mastered ([Template.t])                         The templates that have been mastered.
+# record (%{ "template_name" => integer})         The number of correct answers in a row a user has given for each template.
