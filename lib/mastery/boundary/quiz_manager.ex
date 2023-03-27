8e64ee81119f3f4bc0a56af7ece652048d5e6c8f
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-03-28 00:43:34 +0100

    server API

--- lib/mastery/boundary/quiz_manager.ex
+++ lib/mastery/boundary/quiz_manager.ex
@@ -2,6 +2,18 @@ defmodule Mastery.Boundary.QuizManager do
   alias Mastery.Core.Quiz
   use GenServer
 
+  def build_quiz(manager \\ __MODULE__, quiz_fields) do
+    GenServer.call(manager, {:build_quiz, quiz_fields})
+  end
+
+  def add_template(manager \\ __MODULE__, quiz_title, template_fields) do
+    GenServer.call(manager, {:add_template, quiz_title, template_fields})
+  end
+
+  def lookup_quiz_by_title(manager \\ __MODULE__, quiz_title) do
+    GenServer.call(manager, {:lookup_quiz_by_title, quiz_title})
+  end
+
   def init(quizzes) when is_map(quizzes) do
     {:ok, quizzes}
   end
