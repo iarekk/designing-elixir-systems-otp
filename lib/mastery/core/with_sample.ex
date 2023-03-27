b39ac00d965ab299eef3f40871d39a85503cd95f
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-03-27 23:54:29 +0100

    using the with construct

---
+++ lib/mastery/core/with_sample.ex
@@ -0,0 +1,18 @@
+defmodule Mastery.Core.WithSample do
+  def work(n) do
+    if :rand.uniform(10) == 1 do
+      {:error, "oops!", n}
+    else
+      {:result, :rand.uniform(n * 1000)}
+    end
+  end
+
+  def execute_function_safely_twice(dangerous_work, arg1, arg2) do
+    with {:result, _val1} = good_result1 <- apply(dangerous_work, [arg1]),
+         {:result, _val2} = good_result2 <- apply(dangerous_work, [arg2]) do
+      {:ok, [good_result1, good_result2]}
+    else
+      error_message -> error_message
+    end
+  end
+end
---
+++ with_sample_call_function_twice.exs
@@ -0,0 +1,8 @@
+alias Mastery.Core.WithSample
+
+WithSample.execute_function_safely_twice(&WithSample.work/1, 10, 20) |> IO.inspect
+
+# ➜ mastery git:(main) ✗ mix run with_sample_call_function_twice.exs
+# {:ok, [result: 8756, result: 13658]}
+# ➜ mastery git:(main) ✗ mix run with_sample_call_function_twice.exs
+# {:error, "oops!", 20}
