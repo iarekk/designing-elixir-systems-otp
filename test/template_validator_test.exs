f88d528e264874957b2d9bc4631c7f7e307511de
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-03-31 00:37:36 +0100

    one more test

--- test/template_validator_test.exs
+++ test/template_validator_test.exs
@@ -4,11 +4,23 @@ defmodule TemplateValidatorTest do
   alias Mastery.Boundary.TemplateValidator
 
   describe "test templates pass the validator" do
-    setup [:single_digits, :double_digits]
+    setup [:single_digits, :double_digits, :bad_checker]
 
     test "default test template passes validation", context do
       assert TemplateValidator.errors(context[:single_digits]) == []
       assert TemplateValidator.errors(context[:double_digits]) == []
+
+      assert TemplateValidator.errors(context[:bad_checker]) == [
+               {:checker, "Must be an arity 2 function"}
+             ]
+
+      assert TemplateValidator.errors([]) == [
+               {:name, "Is required"},
+               {:category, "Is required"},
+               {:raw, "Is required"},
+               {:generators, "Is required"},
+               {:checker, "Is required"}
+             ]
     end
   end
 
@@ -18,5 +30,9 @@ defmodule TemplateValidatorTest do
 
   defp double_digits(context) do
     Map.put(context, :double_digits, double_digit_addition_template_fields())
+  end
+
+  defp bad_checker(context) do
+    Map.put(context, :bad_checker, template_fields(checker: fn _ -> :ok end))
   end
 end
