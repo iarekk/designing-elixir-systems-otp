0f72547d88dd0eed26ab5d741bd4b02da5774a7c
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2023-04-27 19:01:55 +0100

    switch pg to localhost

--- mastery_persistence/config/test.exs
+++ mastery_persistence/config/test.exs
@@ -2,7 +2,7 @@ import Config
 
 config :mastery_persistence, MasteryPersistence.Repo,
   database: "mastery_test",
-  hostname: "postgres",
+  hostname: "localhost",
   username: "ectotest",
   password: "ectotest",
   pool: Ecto.Adapters.SQL.Sandbox
